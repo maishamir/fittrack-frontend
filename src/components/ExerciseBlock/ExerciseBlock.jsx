5a17f685c515e859d42837df52b522d035a6da47
Maisha Mir <maisha.mir2014@gmail.com>
2026-02-19 13:24:30 -0800

    exercise block collapsible

--- src/components/ExerciseBlock/ExerciseBlock.jsx
+++ src/components/ExerciseBlock/ExerciseBlock.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ExerciseBlock.scss";
 import SetRow from '../SetRow/SetRow';
 
@@ -12,22 +12,45 @@ import SetRow from '../SetRow/SetRow';
  */
 
 function ExerciseBlock({ exercise, onRepsBlur, onWeightBlur }) {
-    return (
-        <div className='exercise'>
-            <h3 className='exercise__title'>{exercise.exercise.name}</h3>
 
-            <div className="exercise__sets">
-                {exercise.sessionSets.map((set, index) => (
-                    <SetRow
-                        key={set.id}
-                        set={set}
-                        index={index}
-                        onRepsBlur={onRepsBlur}
-                        onWeightBlur={onWeightBlur}
-                    />
-                ))}
+    const [isOpen, setIsOpen] = useState(true);
+
+    const totalSets = exercise.sessionSets.length;
+
+    // hardcoded visual meta for now (no completion logic)
+    const firstSet = exercise.sessionSets[0];
+    const repRange = firstSet.targetExactReps ?? `${firstSet.targetMinReps}-${firstSet.targetMaxReps}`;
+
+    return (
+        <div className={`exercise ${isOpen ? 'exercise--open' : 'exercise--closed'}`}>
+            <div className="exercise__header" onClick={() => setIsOpen(!isOpen)}>
+
+                <div className="exercise__header-left">
+                    <h3 className='exercise__title'>{exercise.exercise.name}</h3>
+                    <div className="exercise__meta">
+                        {totalSets} × {repRange} reps
+                    </div>
+                </div>
+
+                <div className={`exercise__chevron ${isOpen ? 'exercise__chevron--open' : ''}`}>
+                    ▾
+                </div>
+
             </div>
 
+            {isOpen && (
+                <div className="exercise__sets">
+                    {exercise.sessionSets.map((set, index) => (
+                        <SetRow
+                            key={set.id}
+                            set={set}
+                            index={index}
+                            onRepsBlur={onRepsBlur}
+                            onWeightBlur={onWeightBlur}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
