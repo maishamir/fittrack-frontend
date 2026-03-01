f6f5976be81558a7a6272700296a57bdd8d65482
Maisha Mir <maisha.mir2014@gmail.com>
2026-03-01 01:21:35 -0800

    style layout header and footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { Home, Calendar, Plus } from 'lucide-react'
+import { Home, Plus, CalendarDays } from 'lucide-react'
 import "./Footer.scss";
+import { useNavigate } from 'react-router-dom';
 function Footer() {
+    const navigate = useNavigate();
     return (
         <footer className='footer'>
             <ul className='footer__list'>
                 <li>
-                    <div className="footer__item">
-                        <Home className='footer__icon'/>
+                    <div className="footer__item" onClick={() => navigate('/')}>
+                        <Home className='footer__icon' />
                         <small className='footer__label'>Home</small>
                     </div>
                 </li>
                 <li>
                     <div className="footer__item">
-                        <Calendar className='footer__icon'/>
+                        <CalendarDays className='footer__icon' />
                         <small className='footer__label'>Calendar</small>
                     </div>
                 </li>
                 <li>
                     <div className="footer__item">
-                        <Plus className='footer__icon'/>
+                        <Plus className='footer__icon' />
                         <small className='footer__label'>Routines</small>
                     </div>
                 </li>
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "../../../assets/fitTrack-logo.png";
 import "./Header.scss"
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <header className="header">
-      <img src={logo} alt="" />
-      <h1>FitTrack</h1>
+      <img src={logo} alt="FitTrack Logo" onClick={() => navigate('/')} />
+      <h1 onClick={() => navigate('/')}>FitTrack</h1>
     </header>
   );
 }
