96da67c0902260cd5fbf8e18aa78938e46579a93
maishamir <45680002+maishamir@users.noreply.github.com>
2026-04-11 16:20:38 -0700

    style routineCard and fix grammar

--- src/components/Home/RoutineCard/RoutineCard.jsx
+++ src/components/Home/RoutineCard/RoutineCard.jsx
@@ -4,21 +4,20 @@ import "./RoutineCard.scss"
 
 function RoutineCard({ routineName, numExercises, numSets, onStart, isLoading }) {
     return (
-        <div className='routineCard'>
+        <div className='routineCard' onClick={onStart}> 
             <div className="routineCard__left">
                 <h2 className='routineCard__name'>{routineName}</h2>
                 <small className='routineCard__details'>
-                    <p className=''>{numExercises} exercises</p> <span>•</span> <p>{numSets} sets</p>
+                    <p className=''>{numExercises} {numExercises > 1 ? "exercises" : "exercise"} </p> <span>•</span> <p>{numSets} {numSets > 1 ? "sets" : "set"}</p>
                 </small>
             </div>
-            <button
+            <div
                 className='routineCard__start'
-                onClick={onStart}
                 disabled={isLoading}
             >
 
                 <Play color='white' height={20} width={20} />
-            </button>
+            </div>
         </div>
     )
 }
