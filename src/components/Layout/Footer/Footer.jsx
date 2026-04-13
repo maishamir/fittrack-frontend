cc9dd45d3afdc701035718a754410571dd219e55
maishamir <45680002+maishamir@users.noreply.github.com>
2026-04-13 11:23:02 -0700

    indicate active page in footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,33 +1,69 @@
-import React from 'react'
-import { Home, Plus, CalendarDays } from 'lucide-react'
+import React, { useState } from "react";
+import { Home, Plus, CalendarDays } from "lucide-react";
 import "./Footer.scss";
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 function Footer() {
-    const navigate = useNavigate();
-    return (
-        <footer className='footer'>
-            <ul className='footer__list'>
-                <li>
-                    <div className="footer__item" onClick={() => navigate('/')}>
-                        <Home className='footer__icon' />
-                        <small className='footer__label'>Home</small>
-                    </div>
-                </li>
-                <li>
-                    <div className="footer__item" onClick={() => navigate('/calendar')}>
-                        <CalendarDays className='footer__icon' />
-                        <small className='footer__label'>Calendar</small>
-                    </div>
-                </li>
-                <li>
-                    <div className="footer__item" onClick={() => navigate('/routines')}>
-                        <Plus className='footer__icon' />
-                        <small className='footer__label'>Routines</small>
-                    </div>
-                </li>
-            </ul>
-        </footer>
-    )
+  const navigate = useNavigate();
+  const location = useLocation();
+  let activePage = location.pathname;
+
+  return (
+    <footer className="footer">
+      <ul className="footer__list">
+        <li>
+          <div
+            className="footer__item"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <Home
+              className={`footer__label ${activePage === "/" ? "footer__icon--active" : ""}`}
+            />
+            <small
+              className={`footer__label ${activePage === "/" ? "footer__label--active" : ""}`}
+            >
+              Home
+            </small>
+          </div>
+        </li>
+        <li>
+          <div
+            className="footer__item"
+            onClick={() => {
+              navigate("/calendar");
+            }}
+          >
+            <CalendarDays
+              className={`footer__label ${activePage === "/calendar" ? "footer__icon--active" : ""}`}
+            />
+            <small
+              className={`footer__label ${activePage === "/calendar" ? "footer__label--active" : ""}`}
+            >
+              Calendar
+            </small>
+          </div>
+        </li>
+        <li>
+          <div
+            className="footer__item"
+            onClick={() => {
+              navigate("/routines");
+            }}
+          >
+            <Plus
+              className={`footer__label ${activePage === "/routines" ? "footer__icon--active" : ""}`}
+            />
+            <small
+              className={`footer__label ${activePage === "/routines" ? "footer__label--active" : ""}`}
+            >
+              Routines
+            </small>
+          </div>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
