a136bf83fc78260566b383cdabca451aeb5561ab
Maisha Mir <maisha.mir2014@gmail.com>
2026-07-16 00:36:28 -0700

    add basic sign in and sign up

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import logo from "../../../assets/fitTrack-logo.png";
 import "./Header.scss"
 import { useNavigate } from "react-router-dom";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
 function Header() {
   const navigate = useNavigate();
   return (
+    // <header className="header">
+    //   <img src={logo} alt="FitTrack Logo" onClick={() => navigate('/')} />
+    //   <h1 onClick={() => navigate('/')}>FitTrack</h1>
+    // </header>
     <header className="header">
-      <img src={logo} alt="FitTrack Logo" onClick={() => navigate('/')} />
-      <h1 onClick={() => navigate('/')}>FitTrack</h1>
+      <div className="header__content">
+        <img src={logo} alt="FitTrack Logo" onClick={() => { navigate('/') }} />
+        <h1 onClick={() => navigate('/')}>FitTrack</h1>
+      </div>
+
+      <div className="header__auth">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </div>
     </header>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/main.scss";
-
+import { ClerkProvider } from '@clerk/react';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
